7c03927b35483f76b2621726b7115a6244a71004
Fábio Santos <fabiosantosart@gmail.com>
2012-07-24 11:52:12 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+DESC = '''
+Django Queryset Serialization
+because serializing django querysets is more trouble than it's worth
+'''
+
+setup(
+    name='django-queryset-serialization',
+    version=version,
+    description='django-queryset-serialization',
+    long_description=DESC,
+    classifiers=[
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Framework :: Django',
+    'Environment :: Web Environment',
+    ],
+    keywords='django,querysets,serialize,serialization',
+    author=u'Fábio Santos',
+    author_email='fabiosantosart@gmail.com',
+    url='http://github.com/fabiosantoscode/django-queryset-serialization',
+    license='WTFPL',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
