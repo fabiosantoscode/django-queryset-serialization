232eb4a23c24f727ee7bed8c926f2d651d03bcec
Fábio Santos <fabiosantosart@gmail.com>
2012-07-26 09:44:11 +0100

    Forgot to update setup.py to the new version

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
  
-version = '0.0.1'
+version = '0.1.0'
 
 DESC = '''
 Django Queryset Serialization
 because serializing django querysets is more trouble than it's worth
+
+Serializes Django Querysets for later use.
 '''
 
 setup(
